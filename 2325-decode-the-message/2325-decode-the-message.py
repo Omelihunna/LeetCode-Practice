d9668712246ceb6eeca9438ee6bba8fa11043c75
Omelihunna <109868288+Omelihunna@users.noreply.github.com>
2022-10-09 16:30:09 +0100

    Time: 70 ms (32.61%), Space: 13.9 MB (40.62%) - LeetHub

--- 2325-decode-the-message/2325-decode-the-message.py
+++ 2325-decode-the-message/2325-decode-the-message.py
@@ -2,13 +2,11 @@ class Solution:
     def decodeMessage(self, key: str, message: str) -> str:
         alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
         checker = []
-        [checker.append(x) for x in list(key) if x not in checker]
-    
-        my_list = [x for x in checker if x!=' ']
+        [checker.append(x) for x in list(key) if x not in checker and x != " "]
       
         new_dict = {}
-        for x in range(len(my_list)):
-            new_dict[my_list[x]] = alpha[x]
+        for x in range(len(checker)):
+            new_dict[checker[x]] = alpha[x]
         my_str = ''
         for x in message:
             if x == ' ':
