de9e3e41a228cf54a74a85d9470fa224825ec18b
Omelihunna <109868288+Omelihunna@users.noreply.github.com>
2022-10-12 21:00:35 +0100

    Time: 42 ms (80.22%), Space: 13.8 MB (53.69%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -6,5 +6,8 @@ class Solution:
         if n == 1:
             return True
         while n % 2 == 0:
-            n /= 2            
-        return n == 1
+            n /= 2
+        if n == 1:
+            return True
+        else:
+            return False
