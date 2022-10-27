ad5351aaa423f2951f66f74de8ca68a1043ca187
Omelihunna <109868288+Omelihunna@users.noreply.github.com>
2022-10-27 22:32:33 +0100

    Time: 175 ms (23.94%), Space: 14 MB (28.91%) - LeetHub

--- 1154-day-of-the-year/1154-day-of-the-year.py
+++ 1154-day-of-the-year/1154-day-of-the-year.py
@@ -1,17 +1,16 @@
 class Solution:
     def dayOfYear(self, date: str) -> int:
-        date_main = date.split("-")
         days_of_months = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334]
         def CheckLeap(Year):
             if((Year % 400 == 0) or (Year % 100 != 0) and (Year % 4 == 0)):
                 return True
-        if CheckLeap(int(date_main[0])) == True:
+        if CheckLeap(int(date[:4])) == True:
             days_of_months = [0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335]
         months_dict = {}
         for x, y in enumerate(days_of_months):
             months_dict[x + 1] = y
-        month_number = int(date_main[1])
+        month_number = int(date[5:7])
         used_days = months_dict[month_number]
-        current_days = int(date_main[2])
+        current_days = int(date[8:])
         return used_days + current_days
         
