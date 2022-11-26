62846fefa6c07d98f83c9d0ab63f5007377bef01
Omelihunna <109868288+Omelihunna@users.noreply.github.com>
2022-11-26 18:55:49 +0100

    Time: 71 ms (32.45%), Space: 13.9 MB (15.85%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        x = str(n)
+        if x == "1":
+            return True
+        while x != "1":
+            n_list_squared = [(int(i)** 2) for i in x]
+            x = str(sum(n_list_squared))
+            if x == "37":
+                return False
+        return True  
+        
